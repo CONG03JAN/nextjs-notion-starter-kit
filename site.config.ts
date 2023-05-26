7fd54b8f9cceff057f677cfb4c2f7ac72028ec61
Cong <pengc825@gmail.com>
2023-05-26 15:10:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'f30f13cb458147c09328389fcf13bb8a',
+  rootNotionPageId: '1c872bd1a78943c79537361acba19161',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'i am cong',
+  domain: 'https://www.notion.so/iamcong/Next-js-Notion-Starter-Kit-Template-4b10c516855e45faad31d8e32ffd3a85',
+  author: 'CONG03JAN',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
